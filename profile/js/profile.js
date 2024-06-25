a80a3a71d0a4866603bef7ef7a8dd0dc2d7488aa
amito6 <amitchain24@gmail.com>
2024-06-24 23:05:30 -0400

    bookingModal-localStorage

--- profile/js/profile.js
+++ profile/js/profile.js
@@ -1,8 +1,14 @@
 /* All global variable declaration */
 
 let userInfo;
+let user;
+let allBookingData = [];
 let navBrand = document.querySelector(".navbar-brand");
 let logoutBtn = document.querySelector(".logout-btn");
+let bookingForm = document.querySelector(".booking-from");
+let allBInput = bookingForm.querySelectorAll("input");
+let bTextarea = bookingForm.querySelector("textarea");
+let bCloseBtn = document.querySelector(".b-modal-close-btn");
 
 
 
@@ -10,8 +16,8 @@ if(sessionStorage.getItem("__au__") == null){
     window.location = "../index.html"
 }
 userInfo = JSON.parse(sessionStorage.getItem("__au__"));
-console.log(userInfo)
-navBrand.innerHTML = userInfo.hotelName;
+navBrand.innerHTML = userInfo.hotelName; 
+user = userInfo.email.split("@")[0];
  
 
 /* logout coding */
@@ -23,4 +29,21 @@ logoutBtn.onclick = () =>{
         sessionStorage.removeItem("__au__");
         window.location = "../index.html";
     }, 3000);
+}
+
+/* Start booking coding */
+
+bookingForm.onsubmit = (e) =>{
+    e.preventDefault();
+    let data = {notice : bTextarea.value};
+    for(let el of allBInput){
+        key = el.name;
+        data[key] = el.value;
+    }
+    allBookingData.push(data);
+    localStorage.setItem(user+"_allBData",JSON.stringify(allBookingData));
+    swal("Good Job","Booking Success","success")
+    bookingForm.reset("");
+    bCloseBtn.click();
+
 }
